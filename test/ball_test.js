ec95f54a695e9479f06c6890c6e4f0c453ece588
Matthieu Tanguay-Carel <matthieutc@gmail.com>
2015-10-28 20:49:34 +0100

    fixes the test with ball bouncing off computer

--- test/ball_test.js
+++ test/ball_test.js
@@ -2,13 +2,17 @@ describe("Ball", function() {
   it("ball bounces off computer paddle", function() {
     window.canvas = {height:480};
     window.ball = new Ball();
+    window.leftScore = 0;
+    window.rightScore = 0;
+    window.paddle = new Paddle(10, (canvas.height / 2 - 50), 10, 100);
+    window.computer = new Computer();
 
-    var computer = new Computer();
-
+    ball.x = computer.x;
+    ball.x_speed = 1;
+    ball.y = computer.y + 1;
     ball.move();
-    computer.move();
 
-    expect(y_speed).toBe(y_speed *= -1);
+    expect(ball.x_speed).toBe(-2);
   });
 
   it("ball bounces off player paddle", function() {
